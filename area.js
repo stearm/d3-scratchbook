dd87bbd17a69398fb28a65e452400be5583825ff
Stefano Armenes <stefano.armenes@gmail.com>
2017-09-30 16:18:01 +0200

    x axis (wip)

--- area.js
+++ area.js
@@ -1,13 +1,25 @@
 const data = [0, 14, 35, 56, 67, 99, 122, 150, 160, 233, 300, 322, 360];
+const months = d3.range(1, 13);
 const curves = [d3.curveCardinal, d3.curveBasis, d3.curveStep, d3.curveNatural];
 
 const height = 400;
 const width = 1000;
 
+const parseMonth = d3.timeParse('%m');
+
 const svg = d3.select('body').append('svg').attr('width', '100%').attr('height', '100%');
 // 0 <=> height and 360 <=> 0 
-const y = d3.scaleLinear().domain([0, Math.max(...data)]).range([height, 0]);
+const y = d3.scaleLinear().domain([0, d3.max(data)]).range([height, 0]);
 const yAxis = d3.axisLeft(y).ticks(3).tickPadding(8).tickSize(8);
+
+const x = d3.scaleTime()
+  .domain([
+    d3.min(months, (d) => parseMonth(d)),
+    d3.max(months, (d) => parseMonth(d))
+  ])
+  .range([0, width]);
+const xAxis = d3.axisBottom(x);
+
 
 curves.map((curve, index) => {
 
@@ -20,6 +32,7 @@ curves.map((curve, index) => {
   const group = svg.append('g').attr('transform', 'translate(' + (50 + (index * 280)) + ',50)');
 
   group.append('g').attr('class', 'axis y').call(yAxis);
+  group.append('g').attr('class', 'axis x').call(xAxis);
 
   group.append('path')
     .attr('stroke', 'pink')
