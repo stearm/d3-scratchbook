35bb7f47800fb5b774d6142234da047c6c3fcca7
Stefano Armenes <stefano.armenes@gmail.com>
2017-10-06 20:31:51 +0200

    fixing axis and color scales

--- area.js
+++ area.js
@@ -1,6 +1,6 @@
-const data = [0, 14, 35, 56, 67, 99, 122, 150, 160, 233, 300, 322, 360];
+const data = [ 14, 35, 56, 67, 99, 122, 150, 160, 233, 300, 322, 360 ];
 const months = d3.range(1, 13);
-const curves = [d3.curveCardinal, d3.curveBasis, d3.curveStep, d3.curveNatural];
+const curves = [ d3.curveCardinal, d3.curveBasis, d3.curveStep, d3.curveNatural ];
 
 const height = 400;
 const width = 1000;
@@ -8,43 +8,46 @@ const width = 1000;
 const parseMonth = d3.timeParse('%m');
 
 const svg = d3.select('body').append('svg').attr('width', '100%').attr('height', '100%');
+
 // 0 <=> height and 360 <=> 0 
-const y = d3.scaleLinear().domain([0, d3.max(data)]).range([height, 0]);
-const yAxis = d3.axisLeft(y).ticks(3).tickPadding(8).tickSize(8);
+const y = d3.scaleLinear().domain([ 0, d3.max(data) ]).range([ height, 0 ]);
+const yAxis = d3.axisLeft(y); //ticks(3).tickPadding(8).tickSize(8);
 
 const x = d3.scaleTime()
   .domain([
     d3.min(months, (d) => parseMonth(d)),
     d3.max(months, (d) => parseMonth(d))
   ])
-  .range([0, width]);
+  .range([ 0, width ]);
 const xAxis = d3.axisBottom(x);
-
 
 curves.map((curve, index) => {
 
-  const area = d3.area()
-    .x((d, i) => i * 20)
-    .y0(height)
-    .y1((d, i) => y(d))
-    .curve(curve);
+  if (index === 0) {
+    const area = d3.area()
+      .x((d, i) => x(parseMonth(months[i])))
+      .y0(height)
+      .y1((d, i) => y(d))
+      .curve(curve);
 
-  const group = svg.append('g').attr('transform', 'translate(' + (50 + (index * 280)) + ',50)');
+    const group = svg.append('g').attr('transform', `translate(${(50 + (index * 280))} ,50)`);
 
-  group.append('g').attr('class', 'axis y').call(yAxis);
-  group.append('g').attr('class', 'axis x').call(xAxis);
+    group.append('g').attr('class', 'axis y').call(yAxis);
+    group.append('g').attr('class', 'axis x').attr('transform', `translate(0, ${height})`).call(xAxis);
 
-  group.append('path')
-    .attr('stroke', 'pink')
-    .attr('stroke-width', 2)
-    .attr('fill', 'none')
-    .attr('d', area(data));
+    group.append('path')
+      .attr('stroke', 'pink')
+      .attr('stroke-width', 2)
+      .attr('fill', 'none')
+      .attr('d', area(data));
 
-  group.selectAll('circle.group')
-    .data(data)
-    .enter().append('circle')
-    .attr('fill', 'red')
-    .attr('cx', (d, i) => i * 20)
-    .attr('cy', (d, i) => y(d))
-    .attr('r', 2);
+    group.selectAll('circle.group')
+      .data(data)
+      .enter().append('circle')
+      .attr('fill', 'red')
+      .attr('cx', (d, i) => x(parseMonth(months[i])))
+      .attr('cy', (d, i) => y(d))
+      .attr('r', 2);
+  }
+
 });
--- shape.js
+++ shape.js
@@ -1,4 +1,15 @@
 const data = [2, 13, 15, 20, 24];
+
+const fruits = ["orange", "apple", "grape", "banana", "kiwi"];
+
+const colorScale = d3.scaleSequential(d3.interpolateRainbow).domain([0,10]);
+const newColorScale = d3.scaleSequential(d3.interpolateRainbow).domain([1,3]);
+
+// or scalePoint with [min, max] as range => equal distribution
+// or scaleBand with [min, max] and .padding(percentage)
+const x = d3.scaleOrdinal().domain(fruits).range([35, 115, 195, 265, 355]);
+const xAxis = d3.axisBottom(x);
+
 const svg = d3.select('body').append('svg').attr('width', '100%').attr('height', '100%');
 
 // rectangle
@@ -7,15 +18,21 @@ svg.selectAll('rect')
   .enter().append('rect')
   .attr('height', (d, i) => d * 10)
   .attr('width', 70)
-  .attr('fill', 'darkred')
+  .attr('fill', (d,i ) => colorScale(i))
   .attr('x', (d, i) => 80 * i)
   .attr('y', (d, i) => 250 - (d * 10));
+
+svg.append('g')
+  .attr('class', 'x axis hidden')
+  .attr('transform', `translate(0,250)`)
+  .call(xAxis);
 
 // circle
 let computedX = 400;
 svg.selectAll('circle')
   .data(data)
   .enter().append('circle')
+  .attr('fill', (d, i) => newColorScale(i))
   .attr('cx', (d, i) => {
     const cx = computedX;
     computedX += (2 * (d * 2)) + 10;
